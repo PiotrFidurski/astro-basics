e2484f549759141e590e4dadf93913f5d88123aa
pfidurski <crunor55@hotmail.com>
2022-10-21 10:51:24 +0200

    add types for env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly API_PRODUCTION_URL: string;
+  readonly API_DEVELOPMENT_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
